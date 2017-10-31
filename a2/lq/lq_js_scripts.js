610a1ce20d555a217b4d512e184d96f9d7dce72d
Nikos Lionakis <nick.lionakis@gmail.com>
2017-10-31 23:07:12 +0200

    Initialised map,getting geocode,no dynamic content

--- a2/lq/lq_js_scripts.js
+++ a2/lq/lq_js_scripts.js
@@ -8,8 +8,8 @@ function CheckPassEquality(){
   var pass2 = document.getElementById('pass2').value;
   if(pass1 != pass2){
     document.getElementById('pass2').title = "Password Missmatch";
-    document.getElementById('pass2').style.color = "#cc0000";
     var message = document.getElementById('pass2');
+    document.getElementById('pass2').style.color = "#cc0000";
     message.type = "text";
     message.value = "Password Missmatch";
   }
@@ -17,6 +17,7 @@ function CheckPassEquality(){
 /*This function resets the type of input element with id="pass2"
 to 'password'.*/
 function ResetPassword(){
+  'use strict'
   var passVal1 = document.getElementById('pass1').value;
   var passVal2 = document.getElementById('pass2').value;
   if(passVal1 != passVal2){
@@ -25,4 +26,49 @@ function ResetPassword(){
     pass.value = "";
     pass.style.color = "#595959";
   }
+}
+
+var map;
+var geocoder;
+/*Google function to initialize our map.Change it so that it
+only shows map when user presses button*/
+function initMap() {
+  var uluru = {lat: 35.321288, lng: 25.164185};
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 6,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
+
+
+
+//
+function evalAddress() {
+  var country = document.getElementById('country').value;
+  var city = document.getElementById('city').value;
+  var address = document.getElementById('address').value;
+  var geoAddress;
+  if( (city !="") && (address !="") ){
+    geoAddress = address.concat("," ,city, "," , country);
+    alert(geoAddress);
+    geocoder = new google.maps.Geocoder();
+    geocoder.geocode( { 'address': geoAddress}, function(results,status){
+      if (status == 'OK'){
+        alert('status OK');
+        alert(results[0].formatted_address);
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+        });
+      }else {
+        alert('ton hpiame');
+      }
+    });
+  }
+  //alert('valid address');
 }
