03a55d33a4f2f3aec6a3557d39f86abbd541551d
Nikos Lionakis <nick.lionakis@gmail.com>
2017-11-01 11:56:06 +0200

    Getting geocode,new button, onclick init and show map

--- a2/lq/lq_js_scripts.js
+++ a2/lq/lq_js_scripts.js
@@ -30,28 +30,29 @@ function ResetPassword(){
 
 var map;
 var geocoder;
+var geoAddress;
+var marker;
 /*Google function to initialize our map.Change it so that it
 only shows map when user presses button*/
 function initMap() {
   var uluru = {lat: 35.321288, lng: 25.164185};
-  map = new google.maps.Map(document.getElementById('map'), {
+  /*map = new google.maps.Map(document.getElementById('map'), {
     zoom: 6,
     center: uluru
   });
-  var marker = new google.maps.Marker({
+  marker = new google.maps.Marker({
     position: uluru,
     map: map
-  });
+  });*/
 }
 
 
-
-//
+//na dinei to koumpi.Sto koumpi onclick() na emfanizetai o xarths
+//RENAME TO BE THE FUNCTION TO MAKE THE MAP APPEAR WITH THE MARKER
 function evalAddress() {
   var country = document.getElementById('country').value;
   var city = document.getElementById('city').value;
   var address = document.getElementById('address').value;
-  var geoAddress;
   if( (city !="") && (address !="") ){
     geoAddress = address.concat("," ,city, "," , country);
     alert(geoAddress);
@@ -60,8 +61,11 @@ function evalAddress() {
       if (status == 'OK'){
         alert('status OK');
         alert(results[0].formatted_address);
-        map.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
+        map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 6,
+          center: results[0].geometry.location
+        });
+        marker = new google.maps.Marker({
             map: map,
             position: results[0].geometry.location
         });
@@ -70,5 +74,42 @@ function evalAddress() {
       }
     });
   }
-  //alert('valid address');
+}
+//make button.RENAME TO BE THE NEW EVALADDRESS
+function testObjectGeocoder() {
+  var country = document.getElementById('country').value;
+  var city = document.getElementById('city').value;
+  var address = document.getElementById('address').value;
+  if( (city !="") && (address !="") ){
+    geoAddress = address.concat("," ,city, "," , country);
+    geocoder = new google.maps.Geocoder();
+    geocoder.geocode( { 'address': geoAddress}, function(results,status){
+      if (status == 'OK'){
+        alert('STATUS OK');
+        center = results[0].geometry.location;
+        marker =
+        //make new button
+        mainDivElement = document.getElementById('container');
+        newButton = document.createElement('input');
+        newButton.setAttribute('id', 'mapButton');
+        newButton.setAttribute("type", "button");
+        newButton.setAttribute('value', 'Show on Map');
+        newButton.style.display = 'inline-block';
+        newButton.style.width = '15%';
+        newButton.style.padding = '12px 20px';
+        newButton.style.border = '1px solid #ff6600';
+        newButton.style.borderRadius = '7px';
+        newButton.style.backgroundColor = '#ff6600';
+        newButton.style.color = '#d9d9d9';
+        newButton.style.fontSize = '19px';
+        mainDivElement.appendChild(newButton);
+        //instead of evalAddress, make new function to create map
+        //and point to geocoder location
+        newButton.setAttribute('onclick', 'evalAddress()');
+      }else {
+        //give message that we cant find specified address
+        alert('ton hpiame');
+      }
+    });
+  }
 }
