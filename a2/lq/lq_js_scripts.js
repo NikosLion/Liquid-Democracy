82ab97d2451e231563fd32ff9bbd84bda5cb55e2
Nikos Lionakis <nick.lionakis@gmail.com>
2017-11-07 01:57:44 +0200

    Finished UI w/ Camera

--- a2/lq/lq_js_scripts.js
+++ a2/lq/lq_js_scripts.js
@@ -33,8 +33,8 @@ var marker;
 /*Google function to initialize our map.Change it so that it
 only shows map when user presses button*/
 function initMap() {
-  var uluru = {lat: 35.321288, lng: 25.164185};
-  /*map = new google.maps.Map(document.getElementById('map'), {
+  /*var uluru = {lat: 35.321288, lng: 25.164185};
+  map = new google.maps.Map(document.getElementById('map'), {
     zoom: 6,
     center: uluru
   });
@@ -54,16 +54,33 @@ function createMap() {
       if (status == 'OK'){
         alert('status OK');
         alert(results[0].formatted_address);
-        //console.log(results[0].geometry.location);
+        console.log(results[0].formatted_address);
         map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 6,
+          zoom: 13,
           center: results[0].geometry.location
         });
+        //map.setCenter(results[0].geometry.location);
         marker = new google.maps.Marker({
             map: map,
             position: results[0].geometry.location
         });
         console.log(marker.position);
+
+        formCont = document.getElementById('formContainer');
+        mapCont = document.getElementById('map');
+        formCont.style.width = "60%";
+        formCont.style.position = "absolute";
+        formCont.style.left = "0";
+        mapCont.style.width = "40%";
+        mapCont.style.position = "absolute";
+        mapCont.style.right = "0";
+        mapCont.style.bottom = "100px";
+        mapCont.style.margin = '5px 0';
+        var oldButton = document.getElementById('mapButton');
+        if(oldButton != null){
+          oldButton.outerHTML = "";
+          delete oldButton;
+        }
       }else {
         alert('ton hpiame');
       }
@@ -87,23 +104,29 @@ function evalAddress() {
           oldButton.outerHTML = "";
           delete oldButton;
         }
-        //delete map.WORKS
+        //delete map
         var oldMap = document.getElementById('map');
         console.log(oldMap);
         if(oldMap != null){
           document.getElementById('map').innerHTML = "";
         }
-        //
-        center = results[0].geometry.location;
+        //rearange form to 100% width if there is no camera showing
+        oldCamera = document.getElementById('cameraCont');
+        if(oldCamera === null){
+          formCont = document.getElementById('formContainer');
+          formCont.style.width = "100%";
+        }
         //make new button
-        mainDivElement = document.getElementById('map');
+        mainDivElement = document.getElementById('mapBut');
         newButton = document.createElement('input');
         newButton.setAttribute('id', 'mapButton');
         newButton.setAttribute("type", "button");
         newButton.setAttribute('value', 'Show on Map');
-        newButton.style.display = 'inline-block';
-        newButton.style.float = 'center';
-        newButton.style.width = '30%';
+        newButton.setAttribute('onclick', 'createMap()');
+        newButton.style.position = "absolute";
+        newButton.style.bottom = "367px";
+        newButton.style.right = "245px";
+        newButton.style.width = '15%';
         newButton.style.padding = '12px 20px';
         newButton.style.margin = '8px 0';
         newButton.style.outline = '0';
@@ -113,20 +136,87 @@ function evalAddress() {
         newButton.style.backgroundColor = '#ff6600';
         newButton.style.color = '#d9d9d9';
         newButton.style.fontSize = '19px';
-        //instead of evalAddress, make new function to create map
-        //and point to geocoder location
-        mainDivElement.appendChild(newButton);
-        newButton.setAttribute('onclick', 'createMap()');
+        //fix button position if form is narrow
         formCont = document.getElementById('formContainer');
-        mapCont = document.getElementById('map');
-        formCont.style.width = "60%";
-        mapCont.style.width = "40%";
-        mapCont.style.margin = '5px 0';
-        //reorganise page
+        if(formCont.style.width === "60%"){
+          newButton.style.right = "100px";
+          newButton.style.width = "20%"
+        }
+        mainDivElement.appendChild(newButton);
       }else {
         //give message that we cant find specified address
         alert('ton hpiame');
       }
     });
   }
+}
+
+function showRadio(){
+  //na ta ksanadw.Otan exei xarth paei to koumpi pio aristera
+  //get rid of previous 'show camera' button
+  oldButton = document.getElementById('CameraButton');
+  if(oldButton != null){
+    document.getElementById('CameraButton').outerHTML = "";
+  }
+  //hide camera and buttons
+  oldCamera = document.getElementById('cameraCont');
+  oldSnap = document.getElementById('snap');
+  oldUpload = document.getElementById('upload');
+  oldCamera.style.display = "none";
+  oldSnap.style.display = "none";
+  oldUpload.style.display = "none";
+  //if there is a map in the page dont mess with form.
+  oldmap = document.getElementById('map');
+  if(oldmap === null){
+    formCont = document.getElementById('formContainer');
+    formCont.style.width = "100%";
+  }
+  mainDivElement = document.getElementById('cameraBut');
+  newButton = document.createElement('input');
+  newButton.setAttribute('id', 'CameraButton');
+  newButton.setAttribute('type', 'button');
+  newButton.setAttribute('value', "Show Camera");
+  newButton.setAttribute('checked', 'false');
+  newButton.setAttribute('onclick' , 'showCamera()');
+  newButton.style.position = "absolute";
+  newButton.style.top = "19px";
+  newButton.style.right = "245px";
+  newButton.style.width = '15%';
+  newButton.style.padding = '12px 20px';
+  newButton.style.margin = '8px 0';
+  newButton.style.outline = '0';
+  newButton.style.border = '1px solid #ff6600';
+  newButton.style.borderRadius = '7px';
+  newButton.style.bozSizing = 'border-box';
+  newButton.style.backgroundColor = '#ff6600';
+  newButton.style.color = '#d9d9d9';
+  newButton.style.fontSize = '19px';
+  //fix button position if form is narrow
+  formCont = document.getElementById('formContainer');
+  if(formCont.style.width === "60%"){
+    newButton.style.right = "100px";
+    newButton.style.width = "20%"
+  }
+  mainDivElement.appendChild(newButton);
+}
+
+function showCamera(){
+  //get rid of old 'show camera' button
+  oldButton = document.getElementById('CameraButton');
+  if(oldButton != null){
+    document.getElementById('CameraButton').outerHTML = "";
+  }
+  //fix mapButton position
+  formCont = document.getElementById('formContainer');
+  formCont.style.width = "60%";
+  mainDivElement = document.getElementById('cameraCont');
+  oldSnap = document.getElementById('snap');
+  oldUpload = document.getElementById('upload');
+  mainDivElement.style.display = "inline-block";
+  mainDivElement.style.width = "40%";
+  mainDivElement.style.position = "absolute";
+  mainDivElement.style.right = "0";
+  oldSnap.style.display = "inline-block";
+  oldUpload.style.display = "inline-block";
+  faceRec.init();
 }
